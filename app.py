50c3e050beeb6fddd645245085952842abd9fa57
kanchang12 <32091338+kanchang12@users.noreply.github.com>
2024-05-17 21:36:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,29 +5,7 @@ from google.oauth2 import service_account
 
 app = Flask(__name__)
 
-"""
 
-# Define the service account key dictionary
-service_key = {
-  "type": "service_account",
-  "project_id": "<project_id>",
-  "private_key_id": "<private_key_id>",
-  "private_key": "<private_key>",
-  "client_email": "<client_email>",
-  "client_id": "<client_id>",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "<client_x509_cert_url>"
-}
-
-
-# Create credentials using the service account key
-credentials = "XXX"
-
-# Set the credentials globally for all Google Cloud services
-speech_client = speech.SpeechClient(credentials=credentials)
-"""
 
 speech_client = speech.SpeechClient()
 
